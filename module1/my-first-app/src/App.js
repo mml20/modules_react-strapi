ab9ff5ad55ae12895071cd74095202b9fdf82cfe
Matvey Lebedev <59504782+mml20@users.noreply.github.com>
2023-02-01 16:03:38 +0300

    Add my-first-app

---
+++ module1/my-first-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// Здесь импорт картинки
+
+function App() {
+  // Здесь переменные и константы, а также вычисления
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
